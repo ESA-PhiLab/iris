9347d98ed247fe80afa000759ff6c0d462145018
JohnMrziglod <mrzo@gmx.de>
2020-02-16 20:02:06 +0100

    Update test_demo.py

--- iris/tests/test_demo.py
+++ iris/tests/test_demo.py
@@ -5,22 +5,22 @@ import requests
 @pytest.mark.usefixtures('live_server')
 class TestCore:
     def test_public_pages(self):
-        response = requests.get(url_for('main.index', _external=True))
+        response = requests.get(self.url())
         assert response.status_code == 200
 
     def test_required_authentication(self):
         addresses = [
             # admin
-            'admin.users', 'admin.images', 'admin.actions/segmentation',
-            'admin.actions/classification', 'admin.actions/detection',
+            'admin/users', 'admin/images', 'admin/actions/segmentation',
+            'admin/actions/classification', 'admin/actions/detection',
             # user
-            'user.get/current', 'user.show/current', 'user.config',
-            'user.save_config',
+            'user/get/current', 'user/show/current', 'user/config',
+            'user/save_config',
             # segmentation
-            'segmentation.load_mask/1', 'segmentation.save_mask/1',
-            'segmentation.predict_mask/1'
+            'segmentation/load_mask/1', 'segmentation/save_mask/1',
+            'segmentation/predict_mask/1'
         ]
-        for address in map(lambda x: url_for(x, _external=True), addresses):
+        for address in map(self.url, addresses):
             print("Check", address)
             response = requests.get(address)
             if response.status_code == 405:
